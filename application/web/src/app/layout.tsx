04a225c27ac579a95a16ece76e3685441fef6958
Yuki Nakajima <yuki.nakajima@revcomm.co.jp>
2026-02-22 02:14:11 +0900

    title

--- application/web/src/app/layout.tsx
+++ application/web/src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Colon - カスタマイズ可能なインタラクティブボットサービス",
+  title: "Colon - 本格カスタマイズ可能なインタラクティブボットサービス",
   description:
-    "Google Meetに招待できるカスタマイズ可能なリアルタイムインタラクティブボットサービス",
+    "本格カスタマイズ可能なリアルタイムインタラクティブボットサービス",
 };
 
 export default function RootLayout({
